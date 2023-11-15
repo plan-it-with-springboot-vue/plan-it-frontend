0c46e33660119aa93e9f11399739fd5cfe2ab7b4
nayeon <kknyapple@gmail.com>
2023-11-15 12:22:05 +0900

    feat: 특정 날짜 선택 시 Plan 화면 출력을 위한 배열 생성

--- src/components/plan/right/AttractionPlanBox.vue
+++ src/components/plan/right/AttractionPlanBox.vue
@@ -25,7 +25,7 @@
               }}</span>
             </div>
           </div>
-          <div class="line" id="plus-svg">
+          <div class="line" id="trash-svg">
             <div><TrashIconVue /></div>
           </div>
         </div>
@@ -37,15 +37,15 @@
 
 <script setup>
 import { ref, watch } from "vue";
-import LikeVue from "../../../assets/svg/Like.vue";
-import LikeRedVue from "../../../assets/svg/LikeRed.vue";
-import PlusIconVue from "../../../assets/svg/PlusIcon.vue";
 import TrashIconVue from "../../../assets/svg/TrashIcon.vue";
 import {
   useAttractionStore,
-  useCategoryStore,
   useMapStore,
+  usePlanStore,
+  useDateStore,
 } from "../../../stores/store";
+
+const currentDatePlanDetail = ref([]);
 
 const attraction = ref([
   {
@@ -60,90 +60,6 @@ const attraction = ref([
     like: 12,
     isLike: false, // 로그인된 정보로 동작하게 바꿔야함
   },
-  {
-    content_id: 125677,
-    content_type_id: 12,
-    title: "무릉계곡",
-    addr1: "강원도 동해시 삼화로 538",
-    first_image:
-      "http://tong.visitkorea.or.kr/cms/resource/88/1955788_image2_1.jpg",
-    latitude: 38.47884469,
-    longitude: 128.4391216,
-    like: 8,
-    isLike: false,
-  },
-  {
-    content_id: 125782,
-    content_type_id: 12,
-    title: "고석정국민관광지",
-    addr1: "강원도 철원군",
-    first_image:
-      "http://tong.visitkorea.or.kr/cms/resource/62/219162_image2_1.jpg",
-    latitude: 38.44084943,
-    longitude: 128.4547464,
-    like: 5,
-    isLike: false,
-  },
-  {
-    content_id: 125266,
-    content_type_id: 12,
-    title: "국립 청태산자연휴양림",
-    addr1: "강원도 횡성군 둔내면",
-    first_image:
-      "http://tong.visitkorea.or.kr/cms/resource/83/1070183_image2_1.jpg",
-    latitude: 38.34028704,
-    longitude: 128.4999566,
-    like: 22,
-    isLike: false,
-  },
-  {
-    content_id: 125266,
-    content_type_id: 12,
-    title: "국립 청태산자연휴양림",
-    addr1: "강원도 횡성군 둔내면",
-    first_image:
-      "http://tong.visitkorea.or.kr/cms/resource/21/2657021_image2_1.jpg",
-    latitude: 38.51112664,
-    longitude: 128.4191502,
-    like: 12,
-    isLike: false, // 로그인된 정보로 동작하게 바꿔야함
-  },
-  {
-    content_id: 125677,
-    content_type_id: 12,
-    title: "무릉계곡",
-    addr1: "강원도 동해시 삼화로 538",
-    first_image:
-      "http://tong.visitkorea.or.kr/cms/resource/88/1955788_image2_1.jpg",
-    latitude: 38.47884469,
-    longitude: 128.4391216,
-    like: 8,
-    isLike: false,
-  },
-  {
-    content_id: 125782,
-    content_type_id: 12,
-    title: "고석정국민관광지",
-    addr1: "강원도 철원군",
-    first_image:
-      "http://tong.visitkorea.or.kr/cms/resource/62/219162_image2_1.jpg",
-    latitude: 38.44084943,
-    longitude: 128.4547464,
-    like: 5,
-    isLike: false,
-  },
-  {
-    content_id: 125266,
-    content_type_id: 12,
-    title: "국립 청태산자연휴양림",
-    addr1: "강원도 횡성군 둔내면",
-    first_image:
-      "http://tong.visitkorea.or.kr/cms/resource/83/1070183_image2_1.jpg",
-    latitude: 38.34028704,
-    longitude: 128.4999566,
-    like: 22,
-    isLike: false,
-  },
 ]);
 
 const attractionStore = useAttractionStore();
@@ -153,25 +69,26 @@ const showModal = (attractionItem) => {
   attractionStore.showModal(attractionItem);
 };
 
-const toggleLike = (attractionItem) => {
-  attractionItem.isLike = !attractionItem.isLike;
-
-  if (attractionItem.isLike) {
-    attractionItem.like++;
-  } else {
-    attractionItem.like--;
-  }
-};
-
 const mapStore = useMapStore();
 mapStore.addAttractionList(attraction.value);
 
-const categoryStore = useCategoryStore();
+// 계획 추가
+const planStore = usePlanStore();
+const dateStore = useDateStore();
+
 watch(
-  () => categoryStore.selectedCategory,
-  (newVal) => {
-    console.log("Selected Category changed:", newVal);
-  }
+  () => planStore.plan,
+  (newPlan) => {
+    const matchingAttractions = newPlan.planDetail.filter(
+      (detailItem) => detailItem.planDate === dateStore.date
+    );
+
+    currentDatePlanDetail.value = [...matchingAttractions];
+    console.log("attract plan box : ");
+    console.log(currentDatePlanDetail.value);
+  },
+  // watch는 shallow임!! deep으로 해주기
+  { deep: true }
 );
 </script>
 
@@ -187,7 +104,7 @@ watch(
 .line {
   display: flex;
 }
-#plus-svg {
+#trash-svg {
   display: flex;
   justify-content: flex-end;
   margin-right: 0.5rem;
@@ -205,10 +122,6 @@ watch(
   justify-content: space-between;
   height: 4.5rem;
   width: 20.8125rem;
-}
-.like-svg {
-  margin-right: 0.25rem;
-  cursor: pointer;
 }
 span {
   font-size: 0.75rem;
