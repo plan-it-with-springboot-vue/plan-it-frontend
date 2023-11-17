3e8928ab2d4fd67e1420ca4a01eae7fca29a4c52
munhyerin <9993hye@naver.com>
2023-11-17 17:42:42 +0900

    feat: 로그인 UI 구현

---
+++ src/components/user/login/SignUp.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="sign-up-box-container">
+        <div id="sign-up-box-label">
+            <label id="sign-up-label">회원가입</label>
+        </div>
+        <SignUpInput label="이름" type="text" v-model="name"/>
+        <SignUpInput label="아이디" type="text" v-model="id"/>
+        <div>
+            <SignUpInput label="이메일" type="email" v-model="email" placeholder="example@example.com" />
+            <button></button>
+        </div>
+    
+    
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import SignUpInput from '../login/SignUpInputComponents.vue';
+
+const name = ref('');
+const id = ref('');
+const email = ref('');
+</script>
+
+<style scoped>
+#sign-up-box-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+#sign-up-box-label {
+    margin-bottom: 2.94rem;
+}
+#sign-up-label {
+    font-size: 1.5rem;
+    font-weight: 700;
+}
+</style>
---
+++ src/views/SignUpView.vue
@@ -0,0 +1,24 @@
+<template>
+    <TheHeader />
+    <div id="sign-up-container">
+        <div> 
+            <SignUp />
+        </div>
+    </div>
+    <TheFooter />
+</template>
+
+<script setup>
+import TheFooter from '../components/layout/TheFooter.vue';
+import TheHeader from '../components/layout/TheHeader.vue';
+import SignUp from "../components/user/login/SignUp.vue";
+</script>
+
+<style scoped>
+#sign-up-container {
+    display: flex;
+    height: 93vh;
+    justify-content: center;
+    align-items: center;
+}
+</style>
