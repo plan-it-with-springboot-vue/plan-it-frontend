af00d2b75ee43c851b148bad4b17253fa8da8528
hyerin <9993hye@naver.com>
2023-11-18 21:08:26 +0900

    design: 헤더 네비게이션 바 UI 수정

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,7 +1,9 @@
 <template>
   <header>
-    <div id="logo">PLAN IT</div>
     <nav>
+      <div id="logo">
+        <img src="@/assets/image/plan-it-logo.png" alt="PLAN IT">
+      </div>
       <ul>
         <li>홈</li>
         <li>여행 지도</li>
@@ -9,14 +11,35 @@
         <li>커뮤니티</li>
       </ul>
       <ul>
-        <li>로그인</li>
-        <li>회원가입</li>
+        <li v-if="!isLoggedIn">로그인</li>
+        <li v-if="!isLoggedIn">회원가입</li>
+        <li v-else class="dropdown">
+          <div>
+            {{ userName }}님
+          </div>
+          <div id="profile-img">
+            <img src="@/assets/image/profile.png" alt="profile">
+          </div>
+          <div class="dropdown-menu">
+            <ul>
+              <li>마이페이지</li>
+              <li>내 여행 계획 확인</li>
+              <li>좋아요 목록</li>
+            </ul>
+          </div>
+        </li>
       </ul>
     </nav>
   </header>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const isLoggedIn = ref(true); // 로그인 상태 (true 또는 false)
+const userName = ref('김싸피'); // 로그인한 사용자의 이름
+
+</script>
 
 <style scoped>
 header {
@@ -35,18 +58,30 @@ header {
 }
 
 #logo {
-  /* justify-content: center;
-  align-items: center; */
-  text-align: center;
-  width: 8.86088rem;
-  font-size: 1.81rem;
-  font-weight: 700;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-
+#logo img{
+  width: 9rem;
+  height: auto;
+}
+#profile-img {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 0.5rem;
+}
+#profile-img img{
+  width: 1.5rem;
+  height: 1.5rem;
+}
 nav {
   display: flex;
   width: 80rem;
+  height: 3.75rem;
   justify-content: space-between;
+  align-items: center;
   font-size: 1rem;
 }
 
@@ -58,13 +93,54 @@ nav ul {
 }
 
 nav ul li {
+  align-items: center;
   margin-right: 3.61rem;
-}
-nav ul li {
   cursor: pointer;
 }
 
 nav ul li:hover {
   text-decoration: none;
+}
+.dropdown {
+  height: 3.75rem;
+  display: flex;
+  position: relative;
+  cursor: pointer;
+
+}
+.dropdown:hover .dropdown-menu,
+.dropdown-menu:hover {
+  display: flex;
+}
+.dropdown-menu {
+  display: none;
+  position: absolute;
+  top: 100%;
+  left: calc(100% - 160px);
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(76, 76, 76, 0.2);
+  z-index: 1;
+  flex-direction: column;
+}
+
+.dropdown-menu ul {
+  padding: 0;
+  margin: 0;
+  width: 160px;
+  display: flex;
+  flex-direction: column;
+}
+
+.dropdown-menu ul li {
+  width: 128px;
+  padding: 12px 16px;
+  text-align: center;
+  cursor: pointer;
+  white-space: nowrap;
+}
+
+.dropdown-menu ul li:hover {
+  background-color: #f1f1f1;
 }
 </style>
